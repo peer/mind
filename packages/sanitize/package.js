4fb02e6c44b8aa49cdf1b66e109f2b999cf9d3af
Mitar <mitar.git@tnode.com>
2015-10-31 11:53:28 -0700

    Added sanitizing package.

---
+++ packages/sanitize/package.js
@@ -0,0 +1,26 @@
+Package.describe({
+  name: 'sanitize',
+  version: '0.1.0'
+});
+
+Package.onUse(function (api) {
+  api.versionsFrom('1.2.0.2');
+
+  // Core dependencies.
+  api.use([
+    'coffeescript'
+  ]);
+
+  // 3rd party dependencies.
+  api.use([
+    'fermuch:cheerio@0.19.0',
+    'peerlibrary:assert@0.2.5',
+    'peerlibrary:url-utils@0.4.0_2'
+  ]);
+
+  api.export('Sanitize');
+
+  api.addFiles([
+    'sanitize.coffee'
+  ], 'server');
+});
