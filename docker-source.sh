f7d32ee5e983639f153d0fd80dbe93fab4d29f5a
Mitar <mitar.git@tnode.com>
2016-08-23 11:15:17 -0700

    Testing git version fix for Docker.

--- docker-source.sh
+++ docker-source.sh
@@ -1,4 +1,7 @@
+set -e
+
 apt-get update -q -q
 apt-get install --yes --force-yes build-essential python zlib1g-dev zlib1g git
 
+cd /source
 echo "module.exports = '$(git describe --always --dirty=+)'" > /source/packages/core/gitversion.coffee
