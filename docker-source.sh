29bfa0a7f8588a1ef0a8e1854e8ef2a49c62b8af
Mitar <mitar.git@tnode.com>
2016-12-14 19:48:00 -0800

    Adding missing dependency.

--- docker-source.sh
+++ docker-source.sh
@@ -1,7 +1,7 @@
 set -e
 
 apt-get update -q -q
-apt-get install --yes --force-yes build-essential python zlib1g-dev zlib1g git
+apt-get install --yes --force-yes build-essential python zlib1g-dev zlib1g git libgif-dev
 
 cd /source
 echo "module.exports = '$(git describe --always --dirty=+)'" > /source/packages/core/gitversion.coffee
