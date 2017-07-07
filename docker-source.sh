bafdffd9bed32676eed249dfd5ae2c96008cf9f0
Mitar <mitar.github@tnode.com>
2017-07-07 14:08:03 -0700

    Making sure we remove any local development files before we start building the image.

--- docker-source.sh
+++ docker-source.sh
@@ -3,5 +3,7 @@ set -e
 apt-get update -q -q
 apt-get install --yes --force-yes build-essential python zlib1g-dev zlib1g git
 
+rm -rf /source/.meteor/storage
+
 cd /source
 echo "module.exports = '$(git describe --always --dirty=+)'" > /source/packages/core/gitversion.coffee
